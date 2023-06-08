c5f5449d4f620fe2788a2b2cb376b1d93714d696
Alvin Kask <Alvin.Kask@tptlive.ee>
2023-06-08 22:15:45 +0300

    Section_10.54

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,44 +5,48 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model {
+class Post extends Model
+{
     use HasFactory;
 
     protected $guarded = [];
 
     protected $with = ['category', 'author'];
 
-    public function scopeFilter($query, array $filters) {
-        $search = $filters['search'] ?? false;
-        $category = $filters['category'] ?? false;
-        $author = $filters['author'] ?? false;
-
-        $query
-            ->when($search, function ($query, $search) {
-                $query->where(fn($query) => $query->where('title', 'like', '%' . $search . '%')
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query->where(fn($query) =>
+                $query->where('title', 'like', '%' . $search . '%')
                     ->orWhere('body', 'like', '%' . $search . '%')
-                );
-            })
-            ->when($category, function ($query, $category) {
-                $query
-                    ->whereHas('category', function ($query) use ($category) {
-                        $query->where('slug', $category);
-                    });
-            })
-            ->when($author, function ($query, $author) {
-                $query
-                    ->whereHas('author', function ($query) use ($author) {
-                        $query->where('username', $author);
-                    });
-            });
+            )
+        );
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query->whereHas('category', fn ($query) =>
+                $query->where('slug', $category)
+            )
+        );
+
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
+            $query->whereHas('author', fn ($query) =>
+                $query->where('username', $author)
+            )
+        );
     }
 
-    public function category() {
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function author() {
+    public function author()
+    {
         return $this->belongsTo(User::class, 'user_id');
     }
-
 }
